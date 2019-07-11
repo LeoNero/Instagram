0495b657df7d6dccac8f26fe346dc17d62d2f599
LeoNero <leonardofelipenerone@gmail.com>
2019-07-11 16:50:13 -0700

    posts: add infinite scroll

--- app/src/main/java/com/nerone/instagram/fragments/HomeFragment.java
+++ app/src/main/java/com/nerone/instagram/fragments/HomeFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +15,23 @@ import android.view.ViewGroup;
 import com.nerone.instagram.R;
 import com.nerone.instagram.adapters.PostsAdapter;
 import com.nerone.instagram.models.Post;
+import com.nerone.instagram.utils.EndlessRecyclerViewScrollListener;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private LinearLayoutManager linearLayoutManager;
+
     private RecyclerView rvPosts;
     protected SwipeRefreshLayout swipeContainer;
 
     private List<Post> posts;
     protected PostsAdapter adapter;
+
+    private ParseQuery<Post> postQuery;
 
     @Nullable
     @Override
@@ -41,14 +48,23 @@ public class HomeFragment extends Fragment {
 
         posts = new ArrayList<>();
 
-        setSwipeContainer();
         setRecyclerView();
-        getPosts();
+        setSwipeContainer();
+        setEndlessScroll();
+        getPosts(0, true);
     }
 
-     private void setSwipeContainer() {
+    private void setRecyclerView() {
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(linearLayoutManager);
+
+        adapter = new PostsAdapter(getContext(), posts);
+        rvPosts.setAdapter(adapter);
+    }
+
+    private void setSwipeContainer() {
         swipeContainer.setOnRefreshListener(() -> {
-            getPosts();
+            getPosts(0, true);
         });
 
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
@@ -57,21 +73,31 @@ public class HomeFragment extends Fragment {
                 android.R.color.holo_red_light);
     }
 
-    private void setRecyclerView() {
-        adapter = new PostsAdapter(getContext(), posts);
-        rvPosts.setAdapter(adapter);
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+    private void setEndlessScroll() {
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.d("Page", page + "");
+                getPosts(page, false);
+            }
+        };
+
+        rvPosts.addOnScrollListener(scrollListener);
     }
 
-    protected void getPosts() {
+    protected void getPosts(int page, boolean clear) {
         ParseQuery<Post> postQuery = new ParseQuery<>(Post.class);
         postQuery.include(Post.KEY_USER);
         postQuery.setLimit(20);
+        postQuery.setSkip(page * 20);
         postQuery.orderByDescending("createdAt");
 
         postQuery.findInBackground((postsRes, error) -> {
             if (error == null) {
-                adapter.clear();
+                if (clear) {
+                    adapter.clear();
+                }
+
                 adapter.addAll(postsRes);
             }
 
--- app/src/main/java/com/nerone/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/nerone/instagram/fragments/ProfileFragment.java
@@ -6,16 +6,20 @@ import com.parse.ParseUser;
 
 public class ProfileFragment extends HomeFragment {
     @Override
-    protected void getPosts() {
+    protected void getPosts(int page, boolean clear) {
         ParseQuery<Post> postQuery = new ParseQuery<>(Post.class);
         postQuery.include(Post.KEY_USER);
         postQuery.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
         postQuery.setLimit(20);
+        postQuery.setSkip(page * 20);
         postQuery.orderByDescending("createdAt");
 
         postQuery.findInBackground((postsRes, error) -> {
             if (error == null) {
-                adapter.clear();
+                if (clear) {
+                    adapter.clear();
+                }
+
                 adapter.addAll(postsRes);
             }
 
